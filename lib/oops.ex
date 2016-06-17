e813f2b8218e6176fe97e2fd31e5df3d8dc0c30c
Jay Hayes <ur@iamvery.com>
2016-06-17 14:56:25 -0500

    You can specify what your module will act like, oops

--- lib/oops.ex
+++ lib/oops.ex
@@ -1,6 +1,7 @@
 defmodule Oops do
-  defmacro __using__(_opts) do
-    [constructor|[value_func]]
+  defmacro __using__(like: module) do
+    {module, _} = Code.eval_quoted(module)
+    [constructor|[value_func|methods(module)]]
   end
 
   defp constructor do
@@ -13,5 +14,21 @@ defmodule Oops do
     quote do
       def value({__MODULE__, str}), do: str
     end
+  end
+
+  defp methods(module) do
+    for {name, arity} <- module.__info__(:functions) do
+      vars = generate_vars(arity-1)
+      quote do
+        def unquote(name)(unquote_splicing(vars), {__MODULE__, value}) do
+          {__MODULE__, unquote(module).unquote(name)(value, unquote_splicing(vars))}
+        end
+      end
+    end
+  end
+
+  defp generate_vars(0), do: []
+  defp generate_vars(count) do
+    for index <- 1..count, do: Macro.var(:"var#{index}", __MODULE__)
   end
 end
--- test/oops_test.exs
+++ test/oops_test.exs
@@ -3,10 +3,31 @@ defmodule OopsTest do
   doctest Oops
 
   defmodule Stringy do
-    use Oops
+    use Oops, like: String
   end
 
   test "constructor and value" do
     assert Stringy.new("foo").value == "foo"
+  end
+
+  @string Stringy.new("foo")
+
+  test "methods" do
+    assert @string.reverse.value == "oof"
+    assert @string.upcase.value == "FOO"
+    assert @string.match?(~r/oo/).value == true
+    assert @string.ljust(5, ?-).value == "foo--"
+  end
+
+  test "chaining" do
+    assert @string.reverse.upcase.value == "OOF"
+  end
+
+  defmodule Array do
+    use Oops, like: Enum
+  end
+
+  test "array" do
+    assert Array.new([1,2,3]).reverse.value == [3,2,1]
   end
 end
